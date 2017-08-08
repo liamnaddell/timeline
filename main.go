02cb652da2721feec322417a556b86e5fcfa034d
Liam Naddell <liamnprg@gmail.com>
2017-08-08 14:17:17 -0400

    update main.go with new timeline

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
+//© Liam Naddell https://github.com/liamnaddell/timeline
+//licensed under GPL-2.0
+//more info in the LICENSE file at the root of the program
 package main
 
 import "fmt"
 
+const timeline = `
+//examples:↳↱↑↓
+
+  1863: Death of William Thackeray
+  ↑
+<-------------------->
+ ↓
+ 1811: Birth of William Thackeray
+`
+
 func main() {
-	fmt.Println("0<--->")
+	fmt.Println(timeline)
 }
