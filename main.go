4aa930be1b5d113d5a2ac7ee6ac22132d1767c56
Liam Naddell <liamnprg@gmail.com>
2017-08-09 15:02:48 -0400

    more defining of terms and program structure

--- main.go
+++ main.go
@@ -22,8 +22,27 @@ type date struct {
 	Desc string `json:"desc"`
 }
 
+type timemap map[int]dnd
+
+//date no date
+//date without the date field
+//virtual only format
+//struct for now because I might add more fields later
+type dnd struct {
+	Desc string
+}
+
 type timeline struct {
 	Dates []date `json:"dates"`
+}
+
+func (t *timeline) Map() timemap {
+	var final = make(timemap)
+	for i := 0; i < len(t.Dates); i++ {
+		curDate := t.Dates[i].Date
+		final[curDate] = dnd{t.Dates[i].Desc}
+	}
+	return final
 }
 
 var ttl timeline
@@ -36,5 +55,7 @@ func init() {
 }
 
 func main() {
-	fmt.Println(ttl.Dates)
+	timeMap := ttl.Map()
+	fmt.Println(timeMap[1234])
+	timeMap.PrintMap()
 }
--- printTimeline.go
+++ printTimeline.go
@@ -1,1 +1,7 @@
 package main
+
+import "fmt"
+
+func (t timemap) PrintMap() {
+	fmt.Println("hi")
+}
